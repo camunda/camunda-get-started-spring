a484a559c92e75ac5182d9e947876be0f4d62ec3
Sebastian Menski <sebastian.menski@googlemail.com>
2014-04-07 11:53:12 +0200

    Step 2: configure an embedded process engine
    In this step we add the configuration for
    the embedded process engine.
    
    See:
    http://camunda.org/get-started/spring-framework.html#tutorial-configure-an-embedded-process-engine

--- src/main/java/org/camunda/bpm/getstarted/loanapproval/LoanApplicationContext.java
+++ src/main/java/org/camunda/bpm/getstarted/loanapproval/LoanApplicationContext.java
@@ -16,9 +16,87 @@
  */
 package org.camunda.bpm.getstarted.loanapproval;
 
+import javax.sql.DataSource;
+
+import org.camunda.bpm.engine.HistoryService;
+import org.camunda.bpm.engine.ManagementService;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.spring.ProcessEngineFactoryBean;
+import org.camunda.bpm.engine.spring.SpringProcessEngineConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class LoanApplicationContext {
 
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("org.h2.Driver");
+    dataSource.setUrl("jdbc:h2:mem:process-engine;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+    dataSource.setUsername("sa");
+    dataSource.setPassword("");
+    return dataSource;
+  }
+
+  @Bean
+  public PlatformTransactionManager transactionManager(DataSource dataSource) {
+    return new DataSourceTransactionManager(dataSource);
+  }
+
+  @Bean
+  public SpringProcessEngineConfiguration engineConfiguration(DataSource dataSource, PlatformTransactionManager transactionManager) {
+    SpringProcessEngineConfiguration configuration = new SpringProcessEngineConfiguration();
+
+    configuration.setProcessEngineName("engine");
+    configuration.setDataSource(dataSource);
+    configuration.setTransactionManager(transactionManager);
+    configuration.setDatabaseSchemaUpdate("true");
+    configuration.setJobExecutorActivate(false);
+
+    return configuration;
+  }
+
+  @Bean
+  public ProcessEngineFactoryBean engineFactory(SpringProcessEngineConfiguration engineConfiguration) {
+    ProcessEngineFactoryBean factoryBean = new ProcessEngineFactoryBean();
+    factoryBean.setProcessEngineConfiguration(engineConfiguration);
+    return factoryBean;
+  }
+
+  @Bean
+  public ProcessEngine processEngine(ProcessEngineFactoryBean factoryBean) throws Exception {
+    return factoryBean.getObject();
+  }
+
+  @Bean
+  public RepositoryService repositoryService(ProcessEngine processEngine) {
+    return processEngine.getRepositoryService();
+  }
+
+  @Bean
+  public RuntimeService runtimeService(ProcessEngine processEngine) {
+    return processEngine.getRuntimeService();
+  }
+
+  @Bean
+  public TaskService taskService(ProcessEngine processEngine) {
+    return processEngine.getTaskService();
+  }
+
+  @Bean
+  public HistoryService historyService(ProcessEngine processEngine) {
+    return processEngine.getHistoryService();
+  }
+
+  @Bean
+  public ManagementService managementService(ProcessEngine processEngine) {
+    return processEngine.getManagementService();
+  }
 }
