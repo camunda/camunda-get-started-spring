c2eb1da26a22c897a04a5c2e78772babae3ac575
Sebastian Menski <sebastian.menski@googlemail.com>
2014-04-07 11:40:46 +0200

    Bonus: use shared process engine
    In this step we replace the embedded process engine with
    a shared one from a camunda distribution.
    
    See:
    http://camunda.org/get-started/spring-framework.html#bonus-chapters

--- src/main/java/org/camunda/bpm/getstarted/loanapproval/LoanApplicationContext.java
+++ src/main/java/org/camunda/bpm/getstarted/loanapproval/LoanApplicationContext.java
@@ -16,69 +16,35 @@
  */
 package org.camunda.bpm.getstarted.loanapproval;
 
-import javax.sql.DataSource;
-
+import org.camunda.bpm.BpmPlatform;
+import org.camunda.bpm.ProcessEngineService;
 import org.camunda.bpm.engine.HistoryService;
 import org.camunda.bpm.engine.ManagementService;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.RepositoryService;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.TaskService;
-import org.camunda.bpm.engine.spring.ProcessEngineFactoryBean;
-import org.camunda.bpm.engine.spring.SpringProcessEngineConfiguration;
-import org.springframework.beans.factory.annotation.Value;
+import org.camunda.bpm.engine.spring.application.SpringProcessApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class LoanApplicationContext {
 
   @Bean
-  public DataSource dataSource() {
-    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-    dataSource.setDriverClassName("org.h2.Driver");
-    dataSource.setUrl("jdbc:h2:mem:process-engine;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
-    dataSource.setUsername("sa");
-    dataSource.setPassword("");
-    return dataSource;
+  public ProcessEngineService processEngineService() {
+    return BpmPlatform.getProcessEngineService();
+  }
+
+  @Bean(destroyMethod = "")
+  public ProcessEngine processEngine(){
+    return BpmPlatform.getDefaultProcessEngine();
   }
 
   @Bean
-  public PlatformTransactionManager transactionManager(DataSource dataSource) {
-    return new DataSourceTransactionManager(dataSource);
-  }
-
-  @Bean
-  public SpringProcessEngineConfiguration engineConfiguration(
-      DataSource dataSource,
-      PlatformTransactionManager transactionManager,
-      @Value("classpath*:*.bpmn") Resource[] deploymentResources) {
-    SpringProcessEngineConfiguration configuration = new SpringProcessEngineConfiguration();
-
-    configuration.setProcessEngineName("engine");
-    configuration.setDataSource(dataSource);
-    configuration.setTransactionManager(transactionManager);
-    configuration.setDatabaseSchemaUpdate("true");
-    configuration.setJobExecutorActivate(false);
-    configuration.setDeploymentResources(deploymentResources);
-
-    return configuration;
-  }
-
-  @Bean
-  public ProcessEngineFactoryBean engineFactory(SpringProcessEngineConfiguration engineConfiguration) {
-    ProcessEngineFactoryBean factoryBean = new ProcessEngineFactoryBean();
-    factoryBean.setProcessEngineConfiguration(engineConfiguration);
-    return factoryBean;
-  }
-
-  @Bean
-  public ProcessEngine processEngine(ProcessEngineFactoryBean factoryBean) throws Exception {
-    return factoryBean.getObject();
+  public SpringProcessApplication processApplication()
+  {
+    return new SpringProcessApplication();
   }
 
   @Bean
@@ -104,11 +70,6 @@ public class LoanApplicationContext {
   @Bean
   public ManagementService managementService(ProcessEngine processEngine) {
     return processEngine.getManagementService();
-  }
-
-  @Bean
-  public Starter starter() {
-    return new Starter();
   }
 
   @Bean
